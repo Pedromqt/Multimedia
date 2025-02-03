de8abd28597b27b52f59786c9b93e95dfbca24f3
Pedro Trindade <pedrito4315@gmail.com>
2025-02-03 14:56:46 +0000

    +

--- jpeg.py
+++ jpeg.py
@@ -1,3 +1,24 @@
 import matplotlib.pyplot as plt
 import matplotlib.colors as clr
 import numpy as np
+
+
+def encoder():
+    pass
+
+def decoder():
+    pass
+
+def main():
+    fName = "./Multimedia/TP1/imagens/airport.bmp"
+    img = plt.imread(fName) 
+    plt.figure()
+    plt.imshow(img)
+    plt.axis("off")
+    plt.title(fName)
+    plt.show()
+    print(type(img))
+    print(img.shape)
+
+if __name__ == "__main__":
+    main()
