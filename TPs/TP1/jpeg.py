57080e110d5e0f905dbecd7ffa0f6f090ede8c05
Pedro Trindade <pedrito4315@gmail.com>
2025-02-08 14:23:49 +0000

    remove ycbcr

--- TPs/TP1/jpeg.py
+++ TPs/TP1/jpeg.py
@@ -51,8 +51,18 @@ def YCbCr(img):
     Y = img[:,:,0]
     Cb = img[:,:,1]
     Cr = img[:,:,2]
-    return Y,Cb,Cr
+    return Y,Cb,Cr,YCbCr_matrix,YCbCr_matrix_2
     
+def remove_YCbCr(img,YCbCr_matrix,YCbCr_matrix_2):
+    nl,nc,_ = img.shape
+    remove_YCbCr_matrix = np.linalg.inv(YCbCr_matrix)
+    for l in range(nl):
+        for c in range(nc):
+            ycbcr = img[l, c, :]
+            rgb = np.dot(remove_YCbCr_matrix, (ycbcr - YCbCr_matrix_2))
+            img[l,c,:] = rgb
+    return img
+
 def encoder(img,cm_red,cm_green,cm_blue,cm_grey):
     img,added_nl,added_nc = add_padding(img)
     R = img[:,:,0]
@@ -64,7 +74,7 @@ def encoder(img,cm_red,cm_green,cm_blue,cm_grey):
     showImg(B,"Blue",cm_blue) 
     #print("Matriz R")  
     #showSubMatrix(R,8,8,8)
-    Y,Cb,Cr = YCbCr(img)
+    Y,Cb,Cr,YCbCr_matrix,YCbCr_matrix_2 = YCbCr(img)
     showImg(Y,"Y",cm_grey)
     showImg(Cb,"Cb",cm_grey)
     showImg(Cr,"Cr",cm_grey)
@@ -74,14 +84,15 @@ def encoder(img,cm_red,cm_green,cm_blue,cm_grey):
     #print("------------")
     #print("Matriz Cb")
     #showSubMatrix(Cb,8,8,8)
-    return R,G,B,added_nl,added_nc
+    return R,G,B,added_nl,added_nc,YCbCr_matrix,YCbCr_matrix_2
 
-def decoder(R,G,B,added_nl,added_nc):
+def decoder(R,G,B,added_nl,added_nc,YCbCr_matrix,YCbCr_matrix_2):
     nl,nc = R.shape # devolve numero de linhas e colunas
     imgRec = np.zeros((nl,nc,3),dtype=np.uint8)
     imgRec[:,:,0] = R
     imgRec[:,:,1] = G
     imgRec[:,:,2] = B
+    imgRec = remove_YCbCr(imgRec,YCbCr_matrix,YCbCr_matrix_2)
     imgRec = remove_padding(added_nl,added_nc,imgRec)
     return imgRec
 
@@ -96,9 +107,9 @@ def main():
     cm_blue = clr.LinearSegmentedColormap.from_list("blue",[(0,0,0),(0,0,1)], N=256)
     cm_grey = clr.LinearSegmentedColormap.from_list("grey",[(0,0,0),(1,1,1)], N=256)
     
-    R,G,B,added_nl,added_nc = encoder(img,cm_red,cm_green,cm_blue,cm_grey)
+    R,G,B,added_nl,added_nc,YCbCr_matrix,YCbCr_matrix_2 = encoder(img,cm_red,cm_green,cm_blue,cm_grey)
     ###############
-    imgRec = decoder(R,G,B,added_nl,added_nc)
+    imgRec = decoder(R,G,B,added_nl,added_nc,YCbCr_matrix,YCbCr_matrix_2)
     showImg(imgRec,"Imagem Reconstruida / sem padding")
 
 if __name__ == "__main__":
