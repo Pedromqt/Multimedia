169cfb3b10d81a015d591beb4c21170a2ebbe842
João Vaz <joao.vaz1810@gmail.com>
2025-02-09 22:32:26 +0000

    remove_ycbcr feito, verifiquem

--- TPs/TP1/jpeg.py
+++ TPs/TP1/jpeg.py
@@ -21,7 +21,6 @@ def add_padding(img):
     nl,nc,_ = img.shape
     resto_nl = nl % 32
     resto_nc = nc % 32
-    nd = img.ndim
     if(resto_nl != 0):
         add_nl = 32 - resto_nl
         last_line = img[-1:,:,:]
@@ -41,26 +40,25 @@ def remove_padding(added_nl,added_nc,imgRec):
     return imgRec
 
 def YCbCr(img):
+    img = img.astype(np.float32) / 255.0
     YCbCr_matrix = np.array([[0.299,0.587,0.114],[-0.168736,-0.331264,0.5],[0.5,-0.418688,-0.081312]])
     YCbCr_matrix_2 = np.array([0, 128, 128])
-    nl,nc,_ = img.shape
-    for l in range(nl):
-        for c in range(nc):
-            rgb = img[l,c,:]
-            img[l,c,:] = np.dot(YCbCr_matrix,rgb) + YCbCr_matrix_2
+    img = np.dot(img, YCbCr_matrix.T) * 255 + YCbCr_matrix_2
     Y = img[:,:,0]
     Cb = img[:,:,1]
     Cr = img[:,:,2]
     return Y,Cb,Cr,YCbCr_matrix,YCbCr_matrix_2
     
-def remove_YCbCr(img,YCbCr_matrix,YCbCr_matrix_2):
-    nl,nc,_ = img.shape
+def remove_YCbCr(img, YCbCr_matrix, YCbCr_matrix_2):
+    img = img.astype(np.float32)
+    nl, nc, _ = img.shape
     remove_YCbCr_matrix = np.linalg.inv(YCbCr_matrix)
     for l in range(nl):
         for c in range(nc):
-            ycbcr = img[l, c, :]
-            rgb = np.dot(remove_YCbCr_matrix, (ycbcr - YCbCr_matrix_2))
-            img[l,c,:] = rgb
+            ycbcr = img[l, c, :] - YCbCr_matrix_2
+            rgb = np.dot(remove_YCbCr_matrix, ycbcr)
+            img[l, c, :] = rgb
+    img = np.clip(img, 0, 255).astype(np.uint8)
     return img
 
 def encoder(img,cm_red,cm_green,cm_blue,cm_grey):
@@ -84,15 +82,11 @@ def encoder(img,cm_red,cm_green,cm_blue,cm_grey):
     #print("------------")
     #print("Matriz Cb")
     #showSubMatrix(Cb,8,8,8)
-    return R,G,B,added_nl,added_nc,YCbCr_matrix,YCbCr_matrix_2
+    return Y,Cb,Cr,added_nl,added_nc,YCbCr_matrix,YCbCr_matrix_2
 
-def decoder(R,G,B,added_nl,added_nc,YCbCr_matrix,YCbCr_matrix_2):
-    nl,nc = R.shape # devolve numero de linhas e colunas
-    imgRec = np.zeros((nl,nc,3),dtype=np.uint8)
-    imgRec[:,:,0] = R
-    imgRec[:,:,1] = G
-    imgRec[:,:,2] = B
-    imgRec = remove_YCbCr(imgRec,YCbCr_matrix,YCbCr_matrix_2)
+def decoder(Y,Cb,Cr,added_nl,added_nc,YCbCr_matrix,YCbCr_matrix_2):
+    imgRec = np.stack((Y, Cb, Cr), axis=-1)
+    imgRec = remove_YCbCr(imgRec, YCbCr_matrix, YCbCr_matrix_2)
     imgRec = remove_padding(added_nl,added_nc,imgRec)
     return imgRec
 
@@ -107,9 +101,9 @@ def main():
     cm_blue = clr.LinearSegmentedColormap.from_list("blue",[(0,0,0),(0,0,1)], N=256)
     cm_grey = clr.LinearSegmentedColormap.from_list("grey",[(0,0,0),(1,1,1)], N=256)
     
-    R,G,B,added_nl,added_nc,YCbCr_matrix,YCbCr_matrix_2 = encoder(img,cm_red,cm_green,cm_blue,cm_grey)
+    Y,Cb,Cr,added_nl,added_nc,YCbCr_matrix,YCbCr_matrix_2 = encoder(img,cm_red,cm_green,cm_blue,cm_grey)
     ###############
-    imgRec = decoder(R,G,B,added_nl,added_nc,YCbCr_matrix,YCbCr_matrix_2)
+    imgRec = decoder(Y,Cb,Cr,added_nl,added_nc,YCbCr_matrix,YCbCr_matrix_2)
     showImg(imgRec,"Imagem Reconstruida / sem padding")
 
 if __name__ == "__main__":
