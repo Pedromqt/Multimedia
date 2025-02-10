b14fb30ded2557256a59d0ea9bf4784ac124481a
Pedro Trindade <pedrito4315@gmail.com>
2025-02-10 14:31:05 +0000

    fix

--- TPs/TP1/jpeg.py
+++ TPs/TP1/jpeg.py
@@ -42,10 +42,10 @@ def remove_padding(added_nl,added_nc,imgRec):
     return imgRec
 
 def YCbCr(img):
-    img = img.astype(np.float32) / 255.0
+    img = img.astype(np.float32)
     YCbCr_matrix = np.array([[0.299,0.587,0.114],[-0.168736,-0.331264,0.5],[0.5,-0.418688,-0.081312]])
     YCbCr_matrix_2 = np.array([0, 128, 128])
-    img = np.dot(img, YCbCr_matrix.T) * 255 + YCbCr_matrix_2
+    img = np.dot(img, YCbCr_matrix.T) + YCbCr_matrix_2
     Y = img[:,:,0]
     Cb = img[:,:,1]
     Cr = img[:,:,2]
