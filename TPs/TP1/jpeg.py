cb4ecb5f57fddde0bf06c1528ef316b9ebe4c7de
João Vaz <joao.vaz1810@gmail.com>
2025-02-22 14:21:45 +0000

    Revert ".9 feito acho"
    This reverts commit 2c6e7010958f73182f62e8b69c55266d2f17f854.

--- TPs/TP1/jpeg.py
+++ TPs/TP1/jpeg.py
@@ -68,30 +68,55 @@ def upsampling(Y,Cb,Cr):
     imgRec = np.stack((Y,Cb2,Cr2), axis = -1)
     return imgRec
 
-def dpcm_encode(coef_dc):
-    diff = coef_dc.copy()
-
-    for i in range(0, len(coef_dc), 8):
-        for j in range(0, len(coef_dc[0]), 8):
-            if j == 0:
-                if i != 0:
-                    diff[i][j] = coef_dc[i][j] - coef_dc[i-8][len(coef_dc[0])-8-1]
-            else:
-                diff[i][j] = coef_dc[i][j] - coef_dc[i][j-8]
-
-    return diff
+def dpcm_encode(Y_Q, Cb_Q, Cr_Q):
+    Y_Q_blocks = Y_Q.reshape(-1, 8, 8)
+    Y_Q_dc_values = Y_Q_blocks[:, 0, 0]
+    Y_Q_dpcm_values = np.diff(Y_Q_dc_values, prepend=Y_Q_dc_values[0])
+    Y_Q_blocks[:, 0, 0] = Y_Q_dpcm_values
     
+    Cb_Q_blocks = Cb_Q.reshape(-1, 8, 8)
+    Cb_Q_dc_values = Cb_Q_blocks[:, 0, 0]
+    Cb_Q_dpcm_values = np.diff(Cb_Q_dc_values, prepend=Cb_Q_dc_values[0])
+    Cb_Q_blocks[:, 0, 0] = Cb_Q_dc_values
+    
+    Cr_Q_blocks = Cr_Q.reshape(-1, 8, 8)
+    Cr_Q_dc_values = Cr_Q_blocks[:, 0, 0]
+    Cr_Q_dpcm_values = np.diff(Cr_Q_dc_values, prepend=Cr_Q_dc_values[0]) 
+    Cr_Q_blocks[:, 0, 0] = Cr_Q_dc_values
 
-def dpcm_decode(diff):
-    coef_dc = diff.copy()
-    for i in range(0, len(diff), 8):
-        for j in range(0, len(diff[0]), 8):
-            if j == 0:
-                if i != 0:
-                    coef_dc[i][j] = coef_dc[i -8][len(diff[0])-8-1] + diff[i][j]
-            else:
-                coef_dc[i][j] = coef_dc[i][j-8] + diff[i][j]
-    return coef_dc
+    Yb_DPCM =  Y_Q_blocks.reshape(Y_Q.shape)
+    Cbb_DPCM = Cb_Q_blocks.reshape(Cb_Q.shape)
+    Crb_DPCM = Cr_Q_blocks.reshape(Cr_Q.shape)
+    
+    showSubMatrix(Crb_DPCM, 8, 8, 8)
+    
+    showImgLog(Yb_DPCM, "Yb_DCPM", cm_grey)
+    showImgLog(Cbb_DPCM, "Cbb_DCPM", cm_grey)
+    showImgLog(Crb_DPCM, "Crb_DCPM", cm_grey)
+
+    return Yb_DPCM, Cbb_DPCM, Crb_DPCM
+
+def dpcm_decode(Yb_DPCM, Cbb_DPCM, Crb_DPCM):
+    Yb_DPCM_blocks = Yb_DPCM.reshape(-1, 8, 8)
+    Yb_DPCM_dc_values = Yb_DPCM_blocks[:, 0, 0]
+    Yb_DPCM_dpcm_values = np.cumsum(Yb_DPCM_dc_values)
+    Yb_DPCM_blocks[:, 0, 0] = Yb_DPCM_dpcm_values
+    
+    Cbb_DPCM_blocks = Cbb_DPCM.reshape(-1, 8, 8)
+    Cbb_DPCM_dc_values = Cbb_DPCM_blocks[:, 0, 0]
+    Cbb_DPCM_dpcm_values = np.cumsum(Cbb_DPCM_dc_values)
+    Cbb_DPCM_blocks[:, 0, 0] = Cbb_DPCM_dc_values
+    
+    Crb_DPCM_blocks = Crb_DPCM.reshape(-1, 8, 8)
+    Crb_DPCM_dc_values = Crb_DPCM_blocks[:, 0, 0]
+    Crb_DPCM_dpcm_values = np.cumsum(Crb_DPCM_dc_values) 
+    Crb_DPCM_blocks[:, 0, 0] = Crb_DPCM_dc_values
+
+    Yb_Q =  Yb_DPCM_blocks.reshape(Yb_DPCM.shape)
+    Cbb_Q = Cbb_DPCM_blocks.reshape(Cbb_DPCM.shape)
+    Crb_Q = Crb_DPCM_blocks.reshape(Crb_DPCM.shape)
+
+    return Yb_Q, Cbb_Q, Crb_Q
 
 def dct_quantize(Y_dct, Cb_dct, Cr_dct, Qualidade):
     global QY,QCbCr
@@ -263,15 +288,10 @@ def encoder(img):
     showImg(Cb_d,"Cb downsampling 4:2:2",cm_grey)
     showImg(Cr_d,"Cr downsampling 4:2:2",cm_grey)
 
-    dct_calc(Y_d,Cb_d,Cr_d)
+    Y_dct, Cb_dct, Cr_dct = dct_calc(Y_d,Cb_d,Cr_d)
     Y_dct8, Cb_dct8, Cr_dct8 = dct_calc8(Y_d,Cb_d,Cr_d)
     Yb_Q, Cbb_Q, Crb_Q = dct_quantize(Y_dct8, Cb_dct8, Cr_dct8, 75)
-    Yb_DPCM = dpcm_encode(Yb_Q)
-    Cbb_DPCM = dpcm_encode(Cbb_Q)
-    Crb_DPCM = dpcm_encode(Crb_Q)
-    showImgLog(Yb_DPCM, "Yb_DCPM", cm_grey)
-    showImgLog(Cbb_DPCM, "Cbb_DCPM", cm_grey)
-    showImgLog(Crb_DPCM, "Crb_DCPM", cm_grey)
+    Yb_DPCM, Cbb_DPCM, Crb_DPCM = dpcm_encode(Yb_Q, Cbb_Q, Crb_Q)
     
     #Y,Cb,Cr = downsampling(Y,Cb,Cr, 0.5, 0.5)
     #showImg(Y,"Y downsampling 4:2:0",cm_grey)
@@ -286,9 +306,7 @@ def encoder(img):
     return Yb_DPCM, Cbb_DPCM, Crb_DPCM
 
 def decoder(Y, Cb, Cr):
-    Y_Q = dpcm_decode(Y)
-    Cb_Q = dpcm_decode(Cb)
-    Cr_Q = dpcm_decode(Cr)
+    Y_Q,Cb_Q,Cr_Q = dpcm_decode(Y,Cb,Cr)
     Y_d, Cb_d, Cr_d = dct_dequantize(Y_Q,Cb_Q,Cr_Q)
     Y, Cb, Cr = dct_inv8(Y_d, Cb_d, Cr_d)
     imgRec = upsampling(Y, Cb, Cr)
